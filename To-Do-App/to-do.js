bb4ef4757a33186e8380118556445a07b5f01754
Sabry <sabryefrin19@gmail.com>
2024-08-07 22:28:37 +0300

    v0.2 | Added a delete button to each list item in the to-do app, It can check if your input is empty or not.

--- To-Do-App/to-do.js
+++ To-Do-App/to-do.js
@@ -1,15 +1,54 @@
-const userInput = document.querySelector('#to-do-input');
+const textInput = document.querySelector('#text-input');
 const addButton = document.querySelector('#add');
-const listContainer = document.querySelector('#to-do-list');
+const listContainer = document.querySelector('#list-container');
+const label = document.querySelector('#label');
 
-
-
+// this one should be the main function that combines them all
 function createToDoListItem() {
 
-    let text = userInput.value
-    let item = document.createElement('li');
-    listContainer.appendChild(item);
-    item.innerHTML = text;
+    let isInputValid = inputValidation(textInput.value);
+    let text = textInput.value
+
+    if (isInputValid) {
+
+        let listItem = createElement('li', text, listContainer, 'list-item');
+        let deleteButton = createElement('button', 'Delete', listItem, 'delete-button');
+
+        label.innerHTML = 'Please pretend that you saw this idea for the first time and embrace it:';
+        deleteButton.addEventListener('click', deleteListItem);
+    }
+    else {
+
+        label.innerHTML = 'Write something man:';
+    }
+}
+// So this one just so i can create any element in one line
+function createElement(type, text, parentElement, className) {
+    
+    let element = document.createElement(type);
+    element.classList.add(className);
+    element.innerHTML = text;
+    parentElement.appendChild(element);
+    
+    return element;
+}
+
+function inputValidation(input) {
+
+    if (input === '') {
+
+        return false;
+    }
+    else {
+
+        return true;
+    }
+}
+
+function deleteListItem(event) {
+
+    let target = event.target;
+    target.parentNode.remove();
 }
 
 document.addEventListener('DOMContentLoaded', () => {
