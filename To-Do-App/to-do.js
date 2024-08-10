65dadd0a9556cac6782d913c3274a2b71b6caa1d
Sabry <sabryefrin19@gmail.com>
2024-08-10 23:12:04 +0300

    v0.5 | Added some functionality to the to-do app you can now add tasks and mark them as completed or you can delete them.

--- To-Do-App/to-do.js
+++ To-Do-App/to-do.js
@@ -1,30 +1,60 @@
 const textInput = document.querySelector('.text-input');
 const addButton = document.querySelector('.add');
-const listContainer = document.querySelector('.list');
+const list = document.querySelector('.list');
 const label = document.querySelector('.label');
 
-const listItemsCount = 0;
+let listItemsCount = 0;
 // this one should be the main function that combines them all
 function createToDoListItem() {
     let isInputValid = inputValidation(textInput.value);
     let text = textInput.value
     if (isInputValid) {
-        let listItem = createElement('li', text, listContainer, 'list__item');
-        let deleteButton = createElement('button', 'Delete', listItem, 'item__delete-button');
         label.innerHTML = 'Please pretend that you saw this idea for the first time and embrace it';
-        deleteButton.addEventListener('click', deleteListItem);
+        let listItem = createListItem();
+        let checkbox = createCheckbox();
+        let itemText = createListText(text);
+        let delteButton = createDeleteButton();
+        list.appendChild(listItem);
+        listItem.appendChild(checkbox);
+        listItem.appendChild(itemText);
+        listItem.appendChild(delteButton);
     }
     else {
         label.innerHTML = 'Write something dude.';
     }
 }
-// So this one just so i can create any element in one line
-function createElement(type, text, parentElement, className) {
-    let element = document.createElement(type);
+
+function generateID() {
+    return listItemsCount = listItemsCount + 1;
+}
+
+function createCheckbox() {
+    let checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    return checkbox;
+}
+
+function createListItem() {
+    const className = 'list__item';
+    let element = document.createElement('li');
+    element.id = generateID();
     element.classList.add(className);
-    element.innerHTML = text;
-    parentElement.appendChild(element);
     return element;
+}
+
+function createListText(content) {
+    let text = document.createElement('p');
+    text.innerHTML = content;
+    return text;
+}
+
+function createDeleteButton() {
+    const className = 'item__delete-button';
+    let button = document.createElement('button');
+    button.classList.add(className);
+    button.innerHTML = 'Delete';
+    button.addEventListener('click', deleteListItem);
+    return button;
 }
 
 function inputValidation(input) {
