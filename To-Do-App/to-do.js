766907154fe60c9b967c92553d74b4a8d3864cdf
Sabry <sabryefrin19@gmail.com>
2024-08-13 06:33:03 +0300

    v0.8 | Added edit function, you now can edit to-do list items.

--- To-Do-App/to-do.js
+++ To-Do-App/to-do.js
@@ -17,11 +17,16 @@ function createToDoListItem() {
                 </div>
             </li>
         `);
+        const editButton = createHTML(`
+            <button class="item__edit-button">Edit</button>    
+        `)
         const deleteButton = createHTML(`
             <button class="item__delete-button">Delete</button>
         `)
+        item.appendChild(editButton)
         item.appendChild(deleteButton);
         list.appendChild(item);
+        editButton.addEventListener('click', editListItem)
         deleteButton.addEventListener('click', deleteListItem)
         label.innerHTML = 'Please pretend that you saw this idea for the first time and embrace it';
         resetInput(textInput);
@@ -50,8 +55,38 @@ function createHTML(html) {
     return template.content.firstElementChild;
 }
 
-function resetInput (input) {
+function resetInput(input) {
     input.value = '';
+}
+
+function editListItem(event) {
+    let target = event.target;
+    let listItemID = target.parentNode.id;
+    let listItem = document.getElementById(listItemID);
+    let itemTextParagraph = listItem.getElementsByTagName('p')[0];
+    let textParagraphContent = itemTextParagraph.textContent;
+    let container = listItem.getElementsByClassName('item__container')[0];
+    console.log(container)
+    let newInput = createHTML(`
+        <input type="text" value="${textParagraphContent}">
+    `)
+    itemTextParagraph.remove();
+    container.appendChild(newInput);
+    newInput.focus();
+    newInput.setSelectionRange(newInput.value.length, newInput.value.length);
+    newInput.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter')
+        saveEdit(newInput);
+    })
+}
+
+function saveEdit(input) {
+    let newContent = input.value;
+    let newParagraph = createHTML(`
+        <p class="item__paragraph">${newContent}</p>
+    `);
+
+    input.replaceWith(newParagraph);
 }
 
 function deleteListItem(event) {
